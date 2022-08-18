be4628882a53893938bce9e21e2edc005f4212fc
Sajad-Salehi <SajadSolidity@gmail.com>
2022-08-18 16:19:31 +0430

    fixed login.js

--- src/templates/static/js/login.js
+++ src/templates/static/js/login.js
@@ -2,7 +2,7 @@ async function connect() {
 
     if (typeof window.ethereum !== "undefined") {
 
-        await ethereum.request({method: "eth_requestsAccounts"})
+        await window.ethereum.request({method: "eth_requestAccounts"})
         document.getElementById("connectButton").innerHTML = "Connected"
     } else {
 
