7d056afed9385d1a8a6804ec522bfd362191690d
Sajad-Salehi <SajadSolidity@gmail.com>
2022-08-18 14:40:24 +0430

    added login metamask functions

---
+++ src/static/index.js
@@ -0,0 +1,12 @@
+async function connect() {
+
+    if (typeof window.ethereum !== "undefined") {
+
+        await ethereum.request({method: "eth_requestsAccounts"})
+        document.getElementById("connectButton").innerHTML = "Connected"
+    } else {
+
+        document.getElementById("connectButton").innerHTML = 
+          "Please Install Metamask"
+    }
+}
