d5218f701e22387f268fbdd2207218e3abbdc3ee
Sajad-Salehi <SajadSolidity@gmail.com>
2022-08-19 20:53:18 +0430

    added flaskMasseafe handler

--- src/templates/static/js/login.js
+++ src/templates/static/js/login.js
@@ -2,8 +2,24 @@ async function connect() {
 
     if (typeof window.ethereum !== "undefined") {
 
-        await window.ethereum.request({method: "eth_requestAccounts"})
+        await window.ethereum.request({method: "eth_requestAccounts"});
         document.getElementById("connectButton").innerHTML = "Connected"
+        const user_address = ethereum.selectedAddress
+
+        var userInfo = {
+            'user_address': user_address
+        }
+
+        const request = new XMLHttpRequest()
+        request.open('POST', `/processUserLogin/${JSON.stringify(userInfo)}`)
+        request.onload = () => {
+            const flaskMessage = request.responseText
+            console.log(flaskMessage)
+
+        }
+        request.send()
+
+        
     } else {
 
         document.getElementById("connectButton").innerHTML = "Please Install Metamask"
