6a7fe4ddc17ca16942532c81406f6c4d59837a33
Sajad-Salehi <SajadSolidity@gmail.com>
2022-08-19 20:51:31 +0430

    added loginProcess

--- src/app.py
+++ src/app.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, session, url_for, redirect
 
 
 app = Flask(__name__)
 app.debug = True
 app._static_folder = 'templates/static'
+app.config['SECRET_KEY'] = 'mysecret'
 
 
-@app.route("/")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    
+
+    if 'value' in session:
+        return redirect(url_for('home'))
+
     return render_template("login.html")
+
+
+
+@app.route('/processUserLogin/<string:userInfo>', methods=["POST"])
+def processUserLogin(userInfo):
+
+    user_info = json.loads(userInfo)
+    session['value'] = user_info['user_address']
+    print(user_info['user_address'])
+    return "User Info recieved"
+
+
+@app.route("/home")
+def home():
+
+    if 'value' in session:
+        print(session.get('value'))
+        return "hey how are you"
+
+    else:
+        return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
