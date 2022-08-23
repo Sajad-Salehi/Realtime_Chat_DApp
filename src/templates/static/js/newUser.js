2d8e18d137de6acc65af78beb1c124b2e7a8c373
Sajad-Salehi <SajadSolidity@gmail.com>
2022-08-23 18:01:20 +0430

    added new user section

---
+++ src/templates/static/js/newUser.js
@@ -0,0 +1,10 @@
+function submit_profile(){
+
+    const name = document.getElementById('name').value
+    const bio = document.getElementById('bio').value
+    const avatar = document.getElementById('avatar').files
+    console.log(name)
+    console.log(bio)
+    console.log(avatar)
+
+}
