ed577dcf67240c538c986da1625ad5bc2a24d8b9
Sajad-Salehi <SajadSolidity@gmail.com>
2022-09-07 22:11:53 +0430

    add rainbow kit

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,5 +1,9 @@
 import Login from "../components/Login";
 import Head from 'next/head'
+import { providers } from 'ethers';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount, useConnect, useDisconnect, useProvider } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 
 export default function login() {
