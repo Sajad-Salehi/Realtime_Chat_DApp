9918c8b9d4771e3bffcbcdf818c66f37ca156cd0
Sajad-Salehi <SajadSolidity@gmail.com>
2022-09-08 19:07:27 +0430

    added wagmi

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,55 @@
 import '../styles/globals.css'
-import React from 'react';
-import { MoralisProvider } from 'react-moralis';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
+
 
 
 function MyApp({ Component, pageProps }) {
-  return (
+  
 
-    <MoralisProvider initializeOnMount={false}>
-        <Component {...pageProps} />
-    </MoralisProvider>
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps}/>
+      </RainbowKitProvider>
+    </WagmiConfig>
+
   )
 
 }
