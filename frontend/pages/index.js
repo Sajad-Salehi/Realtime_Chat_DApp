5b0527ed0f5bcc1ede20e183359e780f385545bf
Sajad-Salehi <SajadSolidity@gmail.com>
2022-10-07 23:12:22 +0330

    fixed isRegisterd

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -15,6 +15,35 @@ export default function Home() {
   const { connect } = useConnect({connector: new InjectedConnector()})
 
 
+async function isRegisterd(Address = address){
+
+  const res = await fetch(`http://127.0.0.1:8000/authenticate`, {
+    method: 'POST',
+      body: JSON.stringify({
+        "address": Address
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  })
+        
+  res.json().then(resp => {
+
+    console.log(resp)
+    if(resp.status === 'ok'){
+      console.log("1")
+      return true
+    }
+    else{
+      console.log("2")
+      return false
+    }
+  })
+}
+
+
+
+
   return (
     <div>
       <h2>Chat DApp </h2>
@@ -22,7 +51,12 @@ export default function Home() {
       <hr></hr>
       
       {isConnected ?(
-        <NewProfile />
+        
+        <div>
+          
+          {isRegisterd(address) ?(<h1>hi</h1>) : (<NewProfile />)}
+        
+        </div>
 
       ) : (
         <h1>please connect your wallet</h1>
