c2ad17184dfd1ebf56476fe706301884e6913043
Sajad-Salehi <SajadSolidity@gmail.com>
2022-10-07 23:11:25 +0330

    added UserAddress model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,8 +1,12 @@
+from ctypes import addressof
 from pydantic import BaseModel
 
 
 class User(BaseModel):
-
     address: str
     avatar_url: str
     avatar_address: str
+
+
+class UserAddress(BaseModel):
+    address: str
